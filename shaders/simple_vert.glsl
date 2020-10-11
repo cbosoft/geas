76777bea3a289fdca5fc509226b4100e187f7522
Christopher Boyle <chrismjboyle@gmail.com>
2020-10-11 21:23:44 +0100

    Update shaders to show texture

--- shaders/simple_vert.glsl
+++ shaders/simple_vert.glsl
@@ -1,8 +1,13 @@
 #version 330 core
 
 layout (location = 0) in vec4 position;
+// layout (location = 1) in vec4 colour;
+layout (location = 2) in vec2 aTexCoord;
+
+out vec2 TexCoord;
 
 void main()
 {
   gl_Position = position;
+  TexCoord = aTexCoord;
 }
