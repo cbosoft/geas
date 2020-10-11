67b625bb5af7b9087547189f7199e8edd87f203a
Christopher Boyle <chrismjboyle@gmail.com>
2020-10-11 22:50:11 +0100

    Auto use rga or rga when loading texture

--- src/texture/texture.cpp
+++ src/texture/texture.cpp
@@ -20,16 +20,17 @@ Texture::Texture(const ImageData &image)
   glBindTexture(GL_TEXTURE_2D, this->texture_id);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 
+  unsigned int imtype = image.nChannels == 3?GL_RGB:GL_RGBA;
   glTexImage2D(GL_TEXTURE_2D,
       0,
-      GL_RGB,
+      imtype,
       image.width,
       image.height,
       0,
-      GL_RGB,
+      imtype,
       GL_UNSIGNED_BYTE,
       image.data);
 
