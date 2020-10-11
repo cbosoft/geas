9c2464a6c994ea76b8d0932e4347ee6a9d601b72
Christopher Boyle <chrismjboyle@gmail.com>
2020-10-11 22:49:01 +0100

    Flip images when loading

--- src/image/image.cpp
+++ src/image/image.cpp
@@ -4,6 +4,7 @@
 ImageData::ImageData(std::string filename)
 {
   const char *path_cstr = filename.c_str();
+  stbi_set_flip_vertically_on_load(true);
   this->data = stbi_load(path_cstr, &this->width, &this->height, &this->nChannels, 0);
 }
 
