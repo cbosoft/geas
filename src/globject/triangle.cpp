17d688e21ae5bda20d1a5775d7519e240239b704
Christopher Boyle <chrismjboyle@gmail.com>
2020-10-11 22:48:54 +0100

    Update triangle

--- src/globject/triangle.cpp
+++ src/globject/triangle.cpp
@@ -6,27 +6,34 @@
 
 GLTriangle::GLTriangle(Vec2 a, Vec2 b, Vec2 c)
 {
-  float positions[6] = {
-    a.x(), a.y(),
-    b.x(), b.y(),
-    c.x(), c.y()
+  float positions[12] = {
+    a.x(), a.y(), a.x(), a.y(),
+    b.x(), b.y(), b.x(), b.y(),
+    c.x(), c.y(), c.x(), c.y()
   };
 
   glGenBuffers(1, &buffer_id);
   glBindBuffer(GL_ARRAY_BUFFER, buffer_id);
-  glBufferData(GL_ARRAY_BUFFER, 6*sizeof(float), positions, GL_STATIC_DRAW);
+  glBufferData(GL_ARRAY_BUFFER, 12*sizeof(float), positions, GL_STATIC_DRAW);
 
   glGenVertexArrays(1, &attrib_id);
   glBindVertexArray(attrib_id);
-  glEnableVertexAttribArray(0);
   glBindBuffer(GL_ARRAY_BUFFER, buffer_id);
-  glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2*sizeof(float), 0);
+
+  // position attribute
+  glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 4*sizeof(float), 0);
+  glEnableVertexAttribArray(0);
+  // texture coord attribute
+  glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 4*sizeof(float), (void *)(2*sizeof(float)));
+  glEnableVertexAttribArray(1);
 
   ResourceManager &rm = ResourceManager::singleton();
   Shader &vert = rm.get_shader("shaders/simple_vert.glsl");
   Shader &frag = rm.get_shader("shaders/simple_frag.glsl");
   shaderProgram.add_shader(vert);
   shaderProgram.add_shader(frag);
+
+  texture = rm.get_texture("textures/test2.png");
 }
 
 bool GLTriangle::draw(Window &win)
@@ -35,6 +42,21 @@ bool GLTriangle::draw(Window &win)
   glBindBuffer(GL_ARRAY_BUFFER, buffer_id);
   glBindVertexArray(attrib_id);
   shaderProgram.use();
+  texture.use();
   glDrawArrays(GL_TRIANGLES, 0, 3);
   return false;
+}
+
+void GLTriangle::update_vertices(Vec2 a, Vec2 b, Vec2 c)
+{
+  (void) a;
+  (void) b;
+  (void) c;
+  // float positions[6] = {
+  //   a.x(), a.y(),
+  //   b.x(), b.y(),
+  //   c.x(), c.y()
+  // };
+  // glBindBuffer(GL_ARRAY_BUFFER, buffer_id);
+  // glBufferData(GL_ARRAY_BUFFER, 6*sizeof(float), positions, GL_STATIC_DRAW);
 }
--- src/globject/triangle.hpp
+++ src/globject/triangle.hpp
@@ -1,18 +1,21 @@
 #pragma once
 
 #include "../vector/vector.hpp"
-#include "globject.hpp"
 #include "../shader/shader.hpp"
+#include "../texture/texture.hpp"
+#include "globject.hpp"
 
-class GLTriangle final : public GLObject {
+class GLTriangle: public GLObject {
 
   public:
     GLTriangle(Vec2 a, Vec2 b, Vec2 c);
+    void update_vertices(Vec2 a, Vec2 b, Vec2 c);
 
     bool draw(Window &win);
 
   private:
 
     ShaderProgram shaderProgram;
+    Texture texture;
     unsigned int buffer_id, attrib_id;
 };
