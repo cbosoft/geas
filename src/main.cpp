58a3443a2c2d6af39e3a01566ad9704b925ca689
Christopher Boyle <chrismjboyle@gmail.com>
2020-10-13 11:11:25 +0100

    Show off new quad

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,20 @@
 #include <ctime>
 
 #include "window/window.hpp"
-#include "globject/triangle.hpp"
+#include "globject/quad.hpp"
 #include "resourcemanager/resourcemanager.hpp"
 
 int main(void)
 {
   Window &w = window(640, 480, "geas");
 
-  w.push_object(new GLTriangle({-0.5, -0.5}, {0.0, 0.5}, {0.5, -0.5}));
+  Quad *quad1 = Quad::square(2.0);
+  quad1->update_texture("textures/test.png");
+  w.push_object(quad1);
+
+  Quad *quad2 = Quad::square(1.0);
+  quad2->update_texture("textures/test2.png");
+  w.push_object(quad2);
+
   w.run();
 }
