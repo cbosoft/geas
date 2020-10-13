f7b66253515352f6f85eb73a7dc01e8545386c5a
Christopher Boyle <chrismjboyle@gmail.com>
2020-10-13 14:00:15 +0100

    Test out quad anim and position

--- src/main.cpp
+++ src/main.cpp
@@ -10,11 +10,13 @@ int main(void)
   Window &w = window(640, 480, "geas");
 
   Quad *quad1 = Quad::square(2.0);
-  quad1->update_texture("textures/test.png");
+  quad1->update_texture("textures/bg.png");
   w.push_object(quad1);
 
-  Quad *quad2 = Quad::square(1.0);
-  quad2->update_texture("textures/test2.png");
+  Quad *quad2 = Quad::square(0.3);
+  quad2->update_position(Vec3({0.0, 0.9, 0.0}));
+  quad2->update_texture("textures/sprite_sheet.png");
+  quad2->set_loop("run");
   w.push_object(quad2);
 
   w.run();
