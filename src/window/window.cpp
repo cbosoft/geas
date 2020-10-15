373e2dbe9dbfb9ff8b4422a56b0865319ef4bcc4
Chris Boyle <christopher.boyle.101@strath.ac.uk>
2020-10-15 17:43:15 +0100

    Show OpenGL error messages on debug

--- src/window/window.cpp
+++ src/window/window.cpp
@@ -18,6 +18,64 @@ Window *Window::singleton()
   return singleton_ptr;
 }
 
+//#define DEBUG
+
+#ifdef DEBUG
+
+void APIENTRY glDebugOutput(GLenum source,
+                            GLenum type,
+                            unsigned int id,
+                            GLenum severity,
+                            GLsizei length,
+                            const char *message,
+                            const void *userParam)
+{
+  (void) length;
+  (void) userParam;
+
+  // ignore non-significant error/warning codes
+  if(id == 131169 || id == 131185 || id == 131218 || id == 131204) return;
+
+  std::cout << "---------------" << std::endl;
+  std::cout << "Debug message (" << id << "): " <<  message << std::endl;
+
+  switch (source)
+  {
+    case GL_DEBUG_SOURCE_API:             std::cout << "Source: API"; break;
+    case GL_DEBUG_SOURCE_WINDOW_SYSTEM:   std::cout << "Source: Window System"; break;
+    case GL_DEBUG_SOURCE_SHADER_COMPILER: std::cout << "Source: Shader Compiler"; break;
+    case GL_DEBUG_SOURCE_THIRD_PARTY:     std::cout << "Source: Third Party"; break;
+    case GL_DEBUG_SOURCE_APPLICATION:     std::cout << "Source: Application"; break;
+    case GL_DEBUG_SOURCE_OTHER:           std::cout << "Source: Other"; break;
+  }
+  std::cout << std::endl;
+
+  switch (type)
+  {
+    case GL_DEBUG_TYPE_ERROR:               std::cout << "Type: Error"; break;
+    case GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR: std::cout << "Type: Deprecated Behaviour"; break;
+    case GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR:  std::cout << "Type: Undefined Behaviour"; break;
+    case GL_DEBUG_TYPE_PORTABILITY:         std::cout << "Type: Portability"; break;
+    case GL_DEBUG_TYPE_PERFORMANCE:         std::cout << "Type: Performance"; break;
+    case GL_DEBUG_TYPE_MARKER:              std::cout << "Type: Marker"; break;
+    case GL_DEBUG_TYPE_PUSH_GROUP:          std::cout << "Type: Push Group"; break;
+    case GL_DEBUG_TYPE_POP_GROUP:           std::cout << "Type: Pop Group"; break;
+    case GL_DEBUG_TYPE_OTHER:               std::cout << "Type: Other"; break;
+  }
+  std::cout << std::endl;
+
+  switch (severity)
+  {
+    case GL_DEBUG_SEVERITY_HIGH:         std::cout << "Severity: high"; break;
+    case GL_DEBUG_SEVERITY_MEDIUM:       std::cout << "Severity: medium"; break;
+    case GL_DEBUG_SEVERITY_LOW:          std::cout << "Severity: low"; break;
+    case GL_DEBUG_SEVERITY_NOTIFICATION: std::cout << "Severity: notification"; break;
+  } std::cout << std::endl;
+  std::cout << std::endl;
+}
+
+#endif
+
 Window::Window(int w, int h, std::string title)
   :
     w(w), h(h),
@@ -32,10 +90,16 @@ Window::Window(int w, int h, std::string title)
     std::cout << "GLFW " << glfwGetVersionString() << " initialised" << std::endl;
   }
 
+#ifdef MACOS
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
   glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
+#ifdef DEBUG
+  glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, true);
+#endif
 
   glfw_window = glfwCreateWindow(w, h, title.c_str(), nullptr, nullptr);
   if (!glfw_window)
@@ -57,7 +121,22 @@ Window::Window(int w, int h, std::string title)
   }
 
   singleton_ptr = this;
-  std::cerr << "window singleto @ " << singleton_ptr << std::endl;
+  std::cerr << "window singleton @ " << singleton_ptr << std::endl;
+
+#ifdef DEBUG
+  int flags;
+  glGetIntegerv(GL_CONTEXT_FLAGS, &flags);
+  if (flags & GL_CONTEXT_FLAG_DEBUG_BIT) {
+    std::cout << "OpenGL debug messaging enabled" <<std::endl;
+    glEnable(GL_DEBUG_OUTPUT);
+    glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
+    glDebugMessageCallback(glDebugOutput, nullptr);
+    glDebugMessageControl(GL_DONT_CARE, GL_DONT_CARE, GL_DONT_CARE, 0, nullptr, GL_TRUE);
+  }
+  else {
+    std::cout << "WARNING! unable to start OpenGL debug messaging: OpenGL version may be too old." <<std::endl;
+  }
+#endif
 
 }
 
