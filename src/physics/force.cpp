856aec597b859de51a4835ae26d69352b5d0c69c
Christopher Boyle <chrismjboyle@gmail.com>
2020-10-18 22:20:06 +0100

    Fixed bug in force calc

--- src/physics/force.cpp
+++ src/physics/force.cpp
@@ -3,19 +3,18 @@
 
 #include "physics.hpp"
 
-static float simple_exp_potential(float dist)
+static inline float simple_exp_potential(float dist)
 {
-  return std::exp(-5.0*std::abs(dist));
+  return std::exp(-5.0f*std::abs(dist));
 }
 
 Vec2 Physics::get_force_between(const Vec2 &a, const Vec2 &b)
 {
   Vec2 dr = a - b;
-  Vec2 df = Vec2({
-    simple_exp_potential(dr.x()),
-    simple_exp_potential(dr.y()),
-  });
+  float dist = dr.magnitude();
+  float denergy = simple_exp_potential(dist);
+  Vec2 df = dr * denergy;
 
-  debug_msg(Formatter() << "dr=" << dr.to_string() << " -> df=" << df.to_string());
+  debug_msg(Formatter() << df.magnitude() << "," << dr.to_string());
   return df;
 }
