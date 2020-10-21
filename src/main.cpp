d11f89e2e206469ecd55606f2cb962feff1b2314
Christopher Boyle <chrismjboyle@gmail.com>
2020-10-21 20:43:54 +0100

    Delay addition of non-fixed player to game to aid in physics (visual) testing

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,19 @@
 #include <iostream>
+#include <chrono>
 
 #include "game/game.hpp"
 #include "player/player.hpp"
 
+void player_add(Game *game, Player *player, int delay)
+{
+    std::this_thread::sleep_for(std::chrono::milliseconds(delay));
+    std::cerr << "adding player" << std::endl;
+    game->push_object(player);
+}
+
 int main()
 {
     Game *game = Game::singleton();
-
-    {
-        auto *player = new Player();
-        player->absolute_position(Vec3({0, 20, 0.0}));
-        game->push_object(player);
-    }
 
     {
         auto *player = new Player();
@@ -20,6 +22,12 @@ int main()
         player->physics->set_gravity(0.0);
         player->physics->set_fixed();
         game->push_object(player);
+    }
+
+    {
+        auto *player = new Player();
+        player->absolute_position(Vec3({0, 100, 0.0}));
+        std::thread(player_add,game, player, 500).detach();
     }
 
     game->play();
