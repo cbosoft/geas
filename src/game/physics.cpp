7bcf87e8ca129b0ccff4cbd746640cb51fc302de
Christopher Boyle <chrismjboyle@gmail.com>
2020-10-26 15:38:29 +0000

    Move game physics stuff out to own thread

---
+++ src/game/physics.cpp
@@ -0,0 +1,20 @@
+#include "game.hpp"
+
+
+void Game::physics_thread_worker()
+{
+    auto t0 = std::chrono::high_resolution_clock::now();
+    while (this->is_alive()) {
+        // TODO move to physics thread
+        auto t1 = std::chrono::high_resolution_clock::now();
+        std::chrono::duration<float> fs = t1 - t0;
+        t0 = t1;
+        this->time_delta_irl = fs.count();
+        this->time_irl += this->time_delta_irl;
+
+        this->time_delta = this->time_delta_irl * this->time_scale;
+        this->time += this->time_delta;
+        Physics::update();
+    }
+}
+
