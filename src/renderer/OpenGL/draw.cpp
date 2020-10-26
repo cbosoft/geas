b16e7752f41a222dc3966f4c3193664bc8c54804
Christopher Boyle <chrismjboyle@gmail.com>
2020-10-26 21:53:00 +0000

    Add placeholder drawing code to OpenGLRenderer

--- src/renderer/OpenGL/draw.cpp
+++ src/renderer/OpenGL/draw.cpp
@@ -1,6 +1,8 @@
 #include "../../util/exception.hpp"
 #include "../../util/debug.hpp"
 
+#include "shader/shader.hpp"
+
 #include "opengl_renderer.hpp"
 
 void OpenGLRenderer::draw(Scene *scene)
@@ -36,6 +38,14 @@ void OpenGLRenderer::draw(Renderable *renderable)
     if (renderable == nullptr)
         return;
 
-    // TODO: render the renderable
-    debug_msg("warning: unimplemented function OpenGLRenderer::draw() called");
+    Vec3 pos = renderable->absolute_position();
+    Vec4 colour = renderable->colour;
+
+    ShaderProgram *shaderProgram = ShaderProgram::from_file("shaders/simple_vert.glsl", "shaders/block_colour_frag.glsl");
+    shaderProgram->use();
+
+    // create vertex buffer
+    // create vertex attrib array
+
+    debug_msg(Formatter() << "warning: unimplemented function OpenGLRenderer::draw() of renderable at " << pos.to_string());
 }
