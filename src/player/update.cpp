b041ce9c4b27c24d13c9a55df134106b0333a0cc
Christopher Boyle <chrismjboyle@gmail.com>
2020-10-27 15:48:43 +0000

    Add TODO note to delete unnecessary file

--- src/player/update.cpp
+++ src/player/update.cpp
@@ -1,10 +1,6 @@
-#include "../camera/camera.hpp"
 #include "player.hpp"
 
 void Player::update()
 {
-  // apply position to texture
-  //Vec3 abs_position = this->absolute_position();
-  //Vec3 window_position = Camera::main()->world_to_window(abs_position);
-  //this->sprite->update_position(window_position);
+  // TODO check if is necessary
 }
