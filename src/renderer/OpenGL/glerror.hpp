61ff282f79a8e36c78050256648a97ea0de085d4
Christopher Boyle <chrismjboyle@gmail.com>
2020-10-29 09:10:03 +0000

    Add macro for checking errors only in debug mode
    Allows paranoid checking of errors in debug mode, but disables in
    release mode

--- src/renderer/OpenGL/glerror.hpp
+++ src/renderer/OpenGL/glerror.hpp
@@ -5,3 +5,9 @@
 std::string glstrerror(unsigned int e);
 
 void gl_error_check(std::string context);
+
+#ifdef DEBUG
+#define GL_ERROR_CHECK_DBG(S) gl_error_check(S);
+#else
+#define GL_ERROR_CHECK_DBG(S)
+#endif
