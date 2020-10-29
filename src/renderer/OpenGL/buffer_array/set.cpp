9b10fc27ad0551ffd49e6436c96d65ca3a9c6743
Chris Boyle <christopher.boyle.101@strath.ac.uk>
2020-10-29 20:37:12 +0000

    Only invalidate  if vertices actually changed

--- src/renderer/OpenGL/buffer_array/set.cpp
+++ src/renderer/OpenGL/buffer_array/set.cpp
@@ -3,30 +3,46 @@
 void BufferArray::set(int i, const GLVertex &vertex)
 {
     // copy vertex into array
+    GLVertex v = this->_vertices[i];
+
     this->_vertices[i] = vertex;
-    this->_invalid = true;
+
+    if (v != this->_vertices[i])
+        this->_invalid = true;
 }
 
 void BufferArray::set(int i, const Vec3 &position)
 {
+    GLVertex v = this->_vertices[i];
+
     this->_vertices[i].x = position.x();
     this->_vertices[i].y = position.y();
     this->_vertices[i].z = position.z();
-    this->_invalid = true;
+
+    if (v != this->_vertices[i])
+        this->_invalid = true;
 }
 
 void BufferArray::set(int i, const Vec4 &colour)
 {
+    GLVertex v = this->_vertices[i];
+
     this->_vertices[i].r = colour.r();
     this->_vertices[i].g = colour.g();
     this->_vertices[i].b = colour.b();
     this->_vertices[i].a = colour.a();
-    this->_invalid = true;
+
+    if (v != this->_vertices[i])
+        this->_invalid = true;
 }
 
 void BufferArray::set(int i, const Vec2 &texture)
 {
+    GLVertex v = this->_vertices[i];
+
     this->_vertices[i].s = texture.x();
     this->_vertices[i].t = texture.y();
-    this->_invalid = true;
+
+    if (v != this->_vertices[i])
+        this->_invalid = true;
 }
