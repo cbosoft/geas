641d6199befc48d6b011ca67e19689144262ee43
Chris Boyle <christopher.boyle.101@strath.ac.uk>
2020-10-29 14:37:04 +0000

    Add error checking to attrib ctor

--- src/renderer/OpenGL/attrib_array/attrib_array.cpp
+++ src/renderer/OpenGL/attrib_array/attrib_array.cpp
@@ -2,6 +2,7 @@
 
 #include <GL/glew.h>
 
+#include "../../../util/exception.hpp"
 #include "../vertex/vertex.hpp"
 #include "attrib_array.hpp"
 
@@ -10,19 +11,26 @@ AttribArray::AttribArray()
     constexpr unsigned int vsz = sizeof(GLVertex);
 
     glGenVertexArrays(1, &(this->_id));
+    GL_ERROR_CHECK_DBG("AttribArray::ctor() -> after gen array");
+
     glBindVertexArray(this->_id);
+    GL_ERROR_CHECK_DBG("AttribArray::ctor() -> after bind arr");
 
     // position attribute
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, vsz, (void *)offsetof(GLVertex, x));
     glEnableVertexAttribArray(0);
+    GL_ERROR_CHECK_DBG("AttribArray::ctor() -> after enable position");
 
     // color attribute
     glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, vsz, (void *)offsetof(GLVertex, r));
+    GL_ERROR_CHECK_DBG(Formatter() << "AttribArray::ctor() -> after set color " << offsetof(GLVertex, r) << " " << vsz);
     glEnableVertexAttribArray(1);
+    GL_ERROR_CHECK_DBG("AttribArray::ctor() -> after enable color");
 
     // texture coord attribute
     glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, vsz, (void *)offsetof(GLVertex, s));
     glEnableVertexAttribArray(2);
+    GL_ERROR_CHECK_DBG("AttribArray::ctor() -> after enable texture coords");
 }
 
 AttribArray::~AttribArray()
