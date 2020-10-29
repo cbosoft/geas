eeed7ea2113c272a8a864718f20de7a9d91722da
Chris Boyle <christopher.boyle.101@strath.ac.uk>
2020-10-29 20:44:27 +0000

    Add comparison operators to GLVertex struct

--- src/renderer/OpenGL/vertex/vertex.hpp
+++ src/renderer/OpenGL/vertex/vertex.hpp
@@ -1,15 +1,38 @@
 #pragma once
 
-typedef struct {
-  float x;
-  float y;
-  float z;
+typedef struct GLVertex {
+    float x;
+    float y;
+    float z;
 
-  float r;
-  float g;
-  float b;
-  float a;
+    float r;
+    float g;
+    float b;
+    float a;
 
-  float s;
-  float t;
+    float s;
+    float t;
+
+    bool operator==(const GLVertex &other) const
+    {
+        return (
+                (this->x == other.x) &&
+                (this->x == other.x) &&
+                (this->y == other.y) &&
+                (this->z == other.z) &&
+
+                (this->r == other.r) &&
+                (this->g == other.g) &&
+                (this->b == other.b) &&
+                (this->a == other.a) &&
+
+                (this->s == other.s) &&
+                (this->t == other.t)
+                );
+    }
+
+    bool operator!=(const GLVertex &other) const
+    {
+        return !((*this) == other);
+    }
 } GLVertex;
