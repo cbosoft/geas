6cd0a922676641fd561f298fc0533427847a8d47
Christopher Boyle <chrismjboyle@gmail.com>
2020-10-29 09:07:37 +0000

    Fix incorrect type names

--- src/renderer/OpenGL/buffer_array/buffer_array.hpp
+++ src/renderer/OpenGL/buffer_array/buffer_array.hpp
@@ -14,12 +14,12 @@ public:
     BufferArray(const BufferArray &other) =delete; // no copying
     BufferArray(BufferArray &&other);
 
-    void set(int i, const Vertex &vertex);
+    void set(int i, const GLVertex &vertex);
     void set(int i, const Vec3 &position);
     void set(int i, const Vec4 &colour);
     void set(int i, const Vec2 &texture);
 
-    void add(const Vertex &vertex);
+    void add(const GLVertex &vertex);
     void add(const Vec3 &position, const Vec4 &colour, const Vec2 &texture);
     void add(const Vec3 &position, const Vec2 &texture);
     void add(const Vec3 &position, const Vec4 &colour);
@@ -34,6 +34,6 @@ private:
 
     typedef std::lock_guard<std::mutex> lock_guard;
     std::mutex mutex;
-    std::vector<Vertex> _vertices;
+    std::vector<GLVertex> _vertices;
     bool _invalid;
 };
