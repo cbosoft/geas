85d0f7c80485b22c5edec7b171c2ed9b946db00f
Christopher Boyle <chrismjboyle@gmail.com>
2020-10-29 09:11:45 +0000

    Trying out simple shaders

--- shaders/block_colour_frag.glsl
+++ shaders/block_colour_frag.glsl
@@ -1,16 +1,10 @@
 #version 330 core
 
-out vec4 color;
 
-in vec2 TexCoord;
-
-uniform sampler2D ourTexture;
+in vec4 in_colour;
+out vec4 outColor;
 
 void main()
 {
-  vec4 maybe_colour = texture(ourTexture, TexCoord);
-
-  if (maybe_colour.a < 0.1) discard;
-
-  color = maybe_colour;
+  outColor = in_colour;
 }
--- shaders/simple_vert.glsl
+++ shaders/simple_vert.glsl
@@ -1,13 +1,15 @@
 #version 330 core
 
 layout (location = 0) in vec4 position;
-// layout (location = 1) in vec4 colour;
-layout (location = 2) in vec2 aTexCoord;
+layout (location = 1) in vec4 in_colour;
+//layout (location = 2) in vec2 aTexCoord;
 
-out vec2 TexCoord;
+//out vec2 tex_coord;
+out vec4 out_colour;
 
 void main()
 {
   gl_Position = position;
-  TexCoord = aTexCoord;
+  //tex_coord = aTexCoord;
+  out_colour = in_colour;
 }
