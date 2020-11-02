91375f3c01eb85bb16004674c793bc1494165cee
Christopher Boyle <chrismjboyle@gmail.com>
2020-11-02 10:09:14 +0000

    Add (disabled) fps counter

--- src/renderer/OpenGL/run.cpp
+++ src/renderer/OpenGL/run.cpp
@@ -1,3 +1,6 @@
+#include <chrono>
+#include <iostream>
+
 #include "../../game/game.hpp"
 
 #include "opengl_renderer.hpp"
@@ -7,6 +10,8 @@ void OpenGLRenderer::run()
 
     if (!this->initialised)
         this->init();
+
+    // std::chrono::time_point<std::chrono::system_clock> t0 = std::chrono::system_clock::now();
 
     while (!glfwWindowShouldClose(this->glfw_window)) {
 
@@ -25,5 +30,10 @@ void OpenGLRenderer::run()
         if (!this->game->is_alive()) {
             glfwSetWindowShouldClose(this->glfw_window, true);
         }
+
+        // std::chrono::time_point<std::chrono::system_clock> t1 = std::chrono::system_clock::now();
+        // int dt_us = std::chrono::duration_cast<std::chrono::microseconds>(t1 - t0).count();
+        // std::cerr << "fps = " << 1.0/(1e-6*static_cast<float> (dt_us)) << std::endl;
+        // t0 = t1;
     }
 }
