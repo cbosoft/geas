3783bd6c118ba274822d123b5af65bf6fddc12a7
Christopher Boyle <chrismjboyle@gmail.com>
2020-11-03 23:03:53 +0000

    Stressing physics

--- src/main.cpp
+++ src/main.cpp
@@ -20,14 +20,14 @@ int main()
     auto *scene = new Scene();
     game->active_scene(scene);
 
-    Physics::update_rate_hz(400);
+    Physics::update_rate_hz(500);
     Physics::time_scale(1.0f);
     Physics::global_gravity_scale(1e-2);
 
-    const float s = 8.0f;
+    const float s = 16.0f;
     unsigned int ntiles = 0;
-    for (float x = -100.0f; x < 100.0f; x += s) {
-        for (float y = -110.0f; y < -100.0f; y += s) {
+    for (float x = -300.0f; x < 300.0f; x += s) {
+        for (float y = -200.0f; y < -100.0f; y += s) {
             auto *t = new Tile(scene->root, s, true, true);
             t->relative_position(Vec3({x, y, 0.0f}));
             ntiles ++;
