302f62ead8541ff382a1c367c87848799ec705e9
Christopher Boyle <chrismjboyle@gmail.com>
2020-11-03 22:07:31 +0000

    Only jump if in contact with ground

--- src/geas_object/player/update.cpp
+++ src/geas_object/player/update.cpp
@@ -1,3 +1,5 @@
+#include <iostream>
+
 #include "player.hpp"
 
 // Called by physics before update
@@ -5,11 +7,15 @@ void Player::update()
 {
     float y = 0.0f;
     if (this->should_jump) {
-        y = this->jump_force;
+        if (this->get_contact_bottom()) {
+            y = this->jump_force;
+        }
         this->should_jump = false;
     }
 
     float x = this->driving_accel*static_cast<float>(this->driving_direction);
     this->physics->add_impulse(Vec2({x, y}));
+
+    GeasObject::update();
 
 }
