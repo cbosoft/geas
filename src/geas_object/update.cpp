f0fa258e414f9ab0d03fcfab17c223cef669ac36
Christopher Boyle <chrismjboyle@gmail.com>
2020-11-06 21:48:53 +0000

    Reset contact when momentum brings particle away from the surface

--- src/geas_object/update.cpp
+++ src/geas_object/update.cpp
@@ -2,5 +2,21 @@
 
 void GeasObject::update()
 {
-    this->reset_contact();
+    if (this->physics) {
+        const Vec2 &mom = this->physics->get_momentum();
+
+        if (mom.x() > 0.0f) {
+            this->contact_left(false);
+        }
+        else if (mom.x() < 0.0f) {
+            this->contact_right(false);
+        }
+
+        if (mom.y() > 0.0f) {
+            this->contact_bottom(false);
+        }
+        else if (mom.y() < 0.0f) {
+            this->contact_top(false);
+        }
+    }
 }
