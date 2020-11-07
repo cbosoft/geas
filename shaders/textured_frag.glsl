da60c9ccf08fd61c5d40fc42918150d13a454238
Christopher Boyle <chrismjboyle@gmail.com>
2020-11-07 10:41:59 +0000

    Fix variable naming in shader

--- shaders/textured_frag.glsl
+++ shaders/textured_frag.glsl
@@ -3,13 +3,13 @@
 out vec4 color;
 
 in vec4 fragColour;
-in vec2 TexCoord;
+in vec2 fragTexCoord;
 
 uniform sampler2D ourTexture;
 
 void main()
 {
-  vec4 maybe_colour = texture(ourTexture, TexCoord);
+  vec4 maybe_colour = texture(ourTexture, fragTexCoord);
 
   if (maybe_colour.a < 0.1) discard;
 
