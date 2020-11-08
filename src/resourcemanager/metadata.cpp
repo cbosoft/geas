8bc5ab7515d9f2da09a1aa47f5dc46abafcfe416
Christopher Boyle <chrismjboyle@gmail.com>
2020-11-08 17:41:01 +0000

    Add func to read json

--- src/resourcemanager/metadata.cpp
+++ src/resourcemanager/metadata.cpp
@@ -5,19 +5,12 @@
 std::string ResourceManager::get_metadataname(const std::string &filename) const
 {
   std::stringstream ss;
-  ss << this->get_abs_path(filename) << ".json";
+  ss << filename << ".json";
   return ss.str();
 }
 
 json ResourceManager::get_metadata(const std::string &filename) const
 {
   std::string metadataname = this->get_metadataname(filename);
-  std::ifstream ifs(metadataname);
-
-  json rv;
-  if (ifs.good()) {
-    ifs >> rv;
-  }
-
-  return rv;
+  return this->get_json(metadataname);
 }
--- src/resourcemanager/resourcemanager.hpp
+++ src/resourcemanager/resourcemanager.hpp
@@ -17,6 +17,7 @@ class ResourceManager {
     [[nodiscard]] std::string read_text_file(const std::string &file_name) const;
     [[nodiscard]] std::string get_abs_path(const std::string &relative_path) const;
     [[nodiscard]] json get_metadata(const std::string &filename) const;
+    [[nodiscard]] json get_json(const std::string &filename) const;
 
   private:
     ResourceManager();
