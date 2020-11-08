b2122821b1d2b2ffad2453ed95ff47d5336fc9dc
Christopher Boyle <chrismjboyle@gmail.com>
2020-11-08 22:27:51 +0000

    Set player animation based on running or in contact with ground etc

--- src/geas_object/player/update.cpp
+++ src/geas_object/player/update.cpp
@@ -13,6 +13,13 @@ void Player::update()
         this->should_jump = false;
     }
 
+    if (this->driving_direction && this->contact_bottom()) {
+        this->renderable()->set_anim_loop("run");
+    }
+    else {
+        this->renderable()->set_anim_loop("idle");
+    }
+
     float x = this->driving_accel*static_cast<float>(this->driving_direction);
     this->physics->add_impulse(Vec2({x, y}));
 
