4e58bd4e98d32c1bba7229090a8e600fcc4a12c3
Christopher Boyle <chrismjboyle@gmail.com>
2020-11-16 16:49:01 +0000

    Flip player scale when facing left

--- src/geas_object/player/update.cpp
+++ src/geas_object/player/update.cpp
@@ -20,6 +20,12 @@ void Player::update()
         this->renderable()->set_anim_loop("idle");
     }
 
+    if (this->driving_direction) {
+        Vec2 ls = this->local_scale();
+        ls.x(static_cast<float>(this->driving_direction));
+        this->local_scale(ls);
+    }
+
     float x = this->driving_accel*static_cast<float>(this->driving_direction);
     this->physics->add_impulse(Vec2({x, y}));
 
