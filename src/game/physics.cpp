1dc1bd29f4bd61dac5c0bd7e3ce31b2f06b673b9
Christopher Boyle <chrismjboyle@gmail.com>
2020-11-22 16:17:34 +0000

    Cast duration to consistent unit

--- src/game/physics.cpp
+++ src/game/physics.cpp
@@ -6,9 +6,9 @@ void Game::physics_thread_worker()
     auto t0 = std::chrono::high_resolution_clock::now();
     while (this->is_alive()) {
         auto t1 = std::chrono::high_resolution_clock::now();
-        std::chrono::duration<float> fs = t1 - t0;
+        std::chrono::duration<float> fs = std::chrono::duration_cast<std::chrono::microseconds>(t1 - t0);
         t0 = t1;
-        this->time_delta_irl = fs.count();
+        this->time_delta_irl = fs.count(); // us
         this->time_irl += this->time_delta_irl;
 
         this->time_delta = this->time_delta_irl * this->time_scale;
