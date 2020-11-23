03d7e65304764c1074c950094d4f1e320239e79e
Christopher Boyle <chrismjboyle@gmail.com>
2020-11-23 10:46:10 +0000

    Move animation function to animator (controller)

--- src/animator/animator.cpp
+++ src/animator/animator.cpp
@@ -1,10 +1,13 @@
 #include "animator.hpp"
+#include "../animation/animation.hpp"
 #include "../geas_object/geas_object.hpp"
+#include "../resourcemanager/resourcemanager.hpp"
 
 static std::list<Animator *> _animators;
 
 Animator::Animator(GeasObject &owner)
     : owner(owner)
+    , _current_anim(nullptr)
     , _inv_speed(1)
 {
     _animators.push_back(this);
@@ -33,12 +36,72 @@ float Animator::speed() const
     return speed;
 }
 
-void Animator::update(unsigned int frame_number) const
+void Animator::update(unsigned int frame_number)
 {
     if (frame_number % this->_inv_speed == 0) {
-        Renderable *renderable = this->owner.renderable();
-        if (renderable) {
-            renderable->increment_frame();
+
+        if (this->_current_anim) {
+            // get next frame index from animation
+            if (this->_current_anim->update(this->_current_frame, this->_current_frame)) {
+                this->set_frame(this->_current_frame); // set new frame for rendering
+            }
+            else {
+                // the animation is no longer valid; update the state and try again
+                this->check_state();
+                this->update(frame_number);
+                return;
+            }
         }
+
+    }
+}
+
+
+void Animator::set_frame(unsigned int i) const
+{
+    auto *r = this->owner.renderable();
+    if (r) {
+        r->current_frame(i);
+    }
+}
+
+
+void Animator::set_current_anim(const std::string &name)
+{
+    this->_current_anim = this->animations[name];
+}
+
+
+Animation *Animator::current_anim() const
+{
+    return this->_current_anim;
+}
+
+unsigned int Animator::current_frame() const
+{
+    return this->_current_frame;
+}
+
+void Animator::load_animations(const std::string &texture)
+{
+    // TODO: don't read in the file, load it from cache
+    // TODO: cache animations
+
+    json j = ResourceManager::singleton().get_metadata(texture);
+
+    for (const auto &[key, value] : j.items()) {
+        std::string skey = key;
+        if (key == "animation_loops") {
+            this->load_animations_json(value);
+        }
+    }
+}
+
+void Animator::load_animations_json(json loops_obj)
+{
+    for (const auto &[key, value] : loops_obj.items()) {
+        std::string name = key;
+        auto *a = new Animation(name, value);
+        this->animations[name] = a;
     }
 }
--- src/animator/animator.hpp
+++ src/animator/animator.hpp
@@ -16,21 +16,29 @@ public:
     explicit Animator(GeasObject &owner);
     virtual ~Animator();
 
-    void update(unsigned int frame_number) const;
     void load_animations(const std::string &texture);
+
+    void update(unsigned int frame_number);
 
     void speed(float s);
     [[nodiscard]] float speed() const;
 
     void set_current_anim(const std::string &name);
     Animation *current_anim() const;
+
+    unsigned int current_frame() const;
+
 private:
     static const std::list<Animator *> &animators();
 
-    unsigned int _inv_speed;
+    virtual void check_state() =0;
+    void set_frame(unsigned int i) const;
     void load_animations_json(json loops_obj);
 
 
     Animation *_current_anim;
     std::map<std::string, Animation *> animations;
+
+
+    unsigned int _inv_speed, _current_frame;
 };
