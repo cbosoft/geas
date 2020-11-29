fc79e275fe03e6a8b471351737614f7a05de46db
Christopher Boyle <chrismjboyle@gmail.com>
2020-11-29 23:11:05 +0000

    Add main menu ui sub-class

---
+++ src/scene/ui/main_menu/main_menu.cpp
@@ -0,0 +1,19 @@
+#include "main_menu.hpp"
+
+#include "../../../geas_object/tile/tile.hpp"
+
+MainMenu::MainMenu()
+{
+    Font font{"assets/fonts/vict.png"};
+    [[maybe_unused]] auto *background = new Tile(this, 640.0f, "assets/textures/plain_ui_bg.png");
+    auto *continue_button = new SceneTransitButton(this, Vec2(0.0f), "continue", "continue", font, "load_game");
+    auto *quit_button = new QuitButton(this, Vec2({0.0f, -32.0f}), "quit", "quit", font);
+
+    continue_button->set_direction(UIDir_Down, quit_button);
+    quit_button->set_direction(UIDir_Up, continue_button);
+
+
+
+    this->selection_hint = new Tile(this, 16.0f, "assets/textures/arrow.png");
+    this->select(continue_button);
+}
---
+++ src/scene/ui/main_menu/main_menu.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "../ui.hpp"
+#include "../ui_element/elements.hpp"
+
+class MainMenu : public UI {
+public:
+    MainMenu();
+
+};
