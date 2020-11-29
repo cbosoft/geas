49d4b84c33093ed7f528b4bb66b9ea30ebc8ab8f
Christopher Boyle <chrismjboyle@gmail.com>
2020-11-29 12:09:24 +0000

    Add helper func to get from json obj or return default

--- src/util/json.hpp
+++ src/util/json.hpp
@@ -2,3 +2,12 @@
 
 #include <nlohmann/json.hpp>
 using json = nlohmann::json;
+
+template<typename T=std::string, typename K=std::string>
+T json_get_or_default(json j, const K &key, const T &default_value)
+{
+    auto it = j.find(key);
+    if (it != j.end())
+        return *it;
+    return default_value;
+}
