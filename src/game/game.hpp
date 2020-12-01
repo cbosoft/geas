d195e07c708c5c14a775e74268d9509b3bf47779
Christopher Boyle <chrismjboyle@gmail.com>
2020-12-01 21:52:56 +0000

    Decrease default transition delay

--- src/game/game.hpp
+++ src/game/game.hpp
@@ -30,7 +30,7 @@ class Game {
     void active_scene(Scene *scene);
     void active_scene(const std::string& scene_name);
     void add_scene(Scene *scene);
-    void transition_to(Scene *scene, unsigned int delay_ms=800);
+    void transition_to(Scene *scene, unsigned int delay_ms=100);
     void transition_to(const std::string &scene_name, unsigned int delay_ms=800);
 
     void process_input(int key, int scancode, int action, int mods);
