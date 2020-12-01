a19a22254803c4dff3cf20417b3537149b76a3d8
Christopher Boyle <chrismjboyle@gmail.com>
2020-12-01 21:41:04 +0000

    Add melee hitbox to actor

--- src/geas_object/actor/actor.cpp
+++ src/geas_object/actor/actor.cpp
@@ -3,6 +3,17 @@
 Actor::Actor(Transform *parent, float max_HP)
     : GeasObject(parent)
     , max_HP(max_HP)
+    , melee_hitbox(nullptr)
 {
     // do nothing
+}
+
+HitBox *Actor::get_melee_hitbox() const
+{
+    return this->melee_hitbox;
+}
+
+void Actor::set_melee_hitbox(HitBox *hitbox)
+{
+    this->melee_hitbox = hitbox;
 }
--- src/geas_object/actor/actor.hpp
+++ src/geas_object/actor/actor.hpp
@@ -1,13 +1,22 @@
 #pragma once
 
 #include "../geas_object.hpp"
+#include "hitbox/hitbox.hpp"
 
 class Actor : public GeasObject {
 public:
     Actor(Transform *parent, float max_HP);
+
     float HP, max_HP;
 
     virtual void hurt(float v) =0;
     bool is_dead() const;
+
+    HitBox *get_melee_hitbox() const;
+    void set_melee_hitbox(HitBox *hitbox);
+
+private:
+
+    HitBox *melee_hitbox;
 
 };
