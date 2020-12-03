f818bc39fafe527ca5075b7ce926ca74f0ab4f48
Christopher Boyle <chrismjboyle@gmail.com>
2020-12-03 11:00:33 +0000

    disabling lazy de-contacting
    Overlaping colliders seems to have done it

--- src/geas_object/update.cpp
+++ src/geas_object/update.cpp
@@ -2,22 +2,23 @@
 
 void GeasObject::update()
 {
-    const float epsilon = 0.01f;
-    if (this->physics) {
-        const Vec2 &mom = this->physics->get_momentum();
+    this->contact_mask = 0;
+    // const float epsilon = 0.01f;
+    // if (this->physics) {
+    //     const Vec2 &mom = this->physics->get_momentum();
 
-        if (mom.x() > epsilon) {
-            this->contact_left(false);
-        }
-        else if (mom.x() < -epsilon) {
-            this->contact_right(false);
-        }
+    //     if (mom.x() > epsilon) {
+    //         this->contact_left(false);
+    //     }
+    //     else if (mom.x() < -epsilon) {
+    //         this->contact_right(false);
+    //     }
 
-        if (mom.y() > epsilon) {
-            this->contact_bottom(false);
-        }
-        else if (mom.y() < -epsilon) {
-            this->contact_top(false);
-        }
-    }
+    //     if (mom.y() > epsilon) {
+    //         this->contact_bottom(false);
+    //     }
+    //     else if (mom.y() < -epsilon) {
+    //         this->contact_top(false);
+    //     }
+    // }
 }
