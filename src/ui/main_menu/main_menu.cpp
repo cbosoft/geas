8ab994f897e99a601afbb2ce616a85b5743907ff
Christopher Boyle <chrismjboyle@gmail.com>
2020-12-04 10:58:54 +0000

    Use ui setter methods

--- src/ui/main_menu/main_menu.cpp
+++ src/ui/main_menu/main_menu.cpp
@@ -15,12 +15,12 @@ MainMenu::MainMenu()
 
     // TODO: make a selector class
     auto *selector = new GeasObject(this);
-    this->selection_hint = selector;
+    this->set_selection_hint(selector);
     auto *r = new Renderable(selector);
     r->set_texture("assets/textures/selector.png");
     r->size(Vec2({96,32}));
     selector->renderable(r);
-    this->selector_offset = Vec3({-8.0, -8.0, 0.0});
+    this->set_selection_offset(Vec2({-8.0, -8.0}));
 
     this->select(continue_button);
 }
