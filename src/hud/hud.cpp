e0dbf26854f6f88fbf1f6cd744cc985d779c19b2
Christopher Boyle <chrismjboyle@gmail.com>
2020-12-06 17:17:59 +0000

    Change to using simple text in HUD

--- src/hud/hud.cpp
+++ src/hud/hud.cpp
@@ -1,14 +1,14 @@
 #include "hud.hpp"
 
-#include "../text/textbox/textbox.hpp"
+#include "../text/text/text.hpp"
 
 HUD::HUD(Transform *parent)
     :   Transform(parent)
 {
     this->set_static_positioning();
-    this->relative_position(Vec3({-240, 150, 0.0}));
-    this->_text = new TextBox(this, "assets/fonts/vict.png", Vec2({128, 16}), "assets/textures/text_background.png");
-    this->_text->set_text("hp:                            ");
+    this->relative_position(Vec3({-320, -240, 0.0}));
+    this->_text = new Text(this, "assets/fonts/big_serif.png");
+    this->_text->relative_position(Vec3({20, 440}));
     this->t0 = std::chrono::system_clock::now();
 }
 
