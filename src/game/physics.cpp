46d46417194ef78de2a6fb8536946a8d3e73c95e
Christopher Boyle <chrismjboyle@gmail.com>
2020-12-08 21:37:35 +0000

    Tidy up time delta calc in physics thread

--- src/game/physics.cpp
+++ src/game/physics.cpp
@@ -3,13 +3,16 @@
 
 void Game::physics_thread_worker()
 {
-    auto t0 = std::chrono::high_resolution_clock::now();
+    auto start_time = std::chrono::high_resolution_clock::now(), previous_time = start_time;
     while (this->is_alive()) {
-        auto t1 = std::chrono::high_resolution_clock::now();
-        std::chrono::duration<float> fs = std::chrono::duration_cast<std::chrono::microseconds>(t1 - t0);
-        t0 = t1;
-        this->time_delta_irl = fs.count(); // us
-        this->time_irl += this->time_delta_irl;
+        auto now = std::chrono::high_resolution_clock::now();
+        unsigned int dt_us = std::chrono::duration_cast<std::chrono::microseconds>(now - previous_time).count();
+        unsigned int t = std::chrono::duration_cast<std::chrono::milliseconds>(now - start_time).count();
+        float dt_s = static_cast<float>(dt_us)*1e-6f;
+        this->time_delta_irl = dt_s;
+
+        previous_time = now;
+        this->time_irl = static_cast<float>(t);
 
         this->time_delta = this->time_delta_irl * this->time_scale;
         this->time += this->time_delta;
