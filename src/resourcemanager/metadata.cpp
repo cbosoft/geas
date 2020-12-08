dfdc12c1ccccabfa393bb28bc807a51ac0404908
Christopher Boyle <chrismjboyle@gmail.com>
2020-12-08 15:59:01 +0000

    Add helper to get metadata from database that matches pattern

--- src/resourcemanager/metadata.cpp
+++ src/resourcemanager/metadata.cpp
@@ -2,8 +2,20 @@
 #include <fstream>
 #include "resourcemanager.hpp"
 
+
 json ResourceManager::get_metadata(const std::string &filename)
 {
     auto *de = this->get_entry(filename);
     return de->meta();
+}
+
+
+std::vector<json> ResourceManager::get_matching_metadata(const std::string &pattern)
+{
+    auto des = this->_db.get_matching(pattern);
+    std::vector<json> rv;
+    for (const auto &de : des) {
+        rv.push_back(de->meta());
+    }
+    return rv;
 }
--- src/resourcemanager/resourcemanager.hpp
+++ src/resourcemanager/resourcemanager.hpp
@@ -20,6 +20,7 @@ class ResourceManager {
     [[nodiscard]] std::string read_text_file(const std::string &file_name);
     [[nodiscard]] const std::vector<char> &read_binary_file(const std::string &file_name);
     [[nodiscard]] json get_metadata(const std::string &filename);
+    [[nodiscard]] std::vector<json> get_matching_metadata(const std::string &pattern);
 
   private:
     ResourceManager();
