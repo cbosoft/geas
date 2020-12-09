b1a39d9d09f1a67f49f2f5a85a0fb6befea7da6e
Christopher Boyle <chrismjboyle@gmail.com>
2020-12-09 11:15:00 +0000

    Fix deadlock - dont have lock on overlong
    Only use lock when exactly interacting with the game obj: copying the
    events out and merging survivors back in rather than maintaining the
    lock throughout the event processing section

--- src/game/events.cpp
+++ src/game/events.cpp
@@ -11,25 +11,31 @@ void Game::push_event(Event *event)
 void Game::events_thread_worker()
 {
     while (this->is_alive()) {
+        std::list<Event *> events_copy;
         {
             std::scoped_lock<std::mutex> _sl(this->mutex);
-            std::list<Event *> surviving;
-            for (const auto &event : this->events_queue) {
-                if (event->should_run(this->get_time())) {
-                    event->run();
-                }
-
-                if (event->should_delete()) {
-                    delete event;
-                }
-                else {
-                    surviving.push_back(event);
-                }
-
-            }
-            this->events_queue = surviving;
-
+            events_copy.merge(this->events_queue);
         }
+
+        std::list<Event *> surviving;
+        for (const auto &event : events_copy) {
+            if (event->should_run(this->get_time())) {
+                event->run();
+            }
+
+            if (event->should_delete()) {
+                delete event;
+            }
+            else {
+                surviving.push_back(event);
+            }
+        }
+
+        {
+            std::scoped_lock<std::mutex> _sl(this->mutex);
+            this->events_queue.merge(surviving);
+        }
+
 
         // time delay without lock on to stop this thread from lockingup the game object
         std::this_thread::sleep_for(std::chrono::milliseconds(100));
